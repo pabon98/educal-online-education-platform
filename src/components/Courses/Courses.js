ee84842149982000aa692b0d13a14f0b4f8c45c5
pabon98 <hasinanjumpabon@gmail.com>
2021-10-31 12:14:02 +0600

    added

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -14,7 +14,7 @@ const Courses = () => {
     return (
       <div className="course">
         <div className="row g-2 my-3">
-          <h1>Find the right online courses for you</h1>
+          <h1>Find the right & best online courses for you</h1>
           {courses.slice(0,4).map((course) => (
             <div className="card ms-5 mt-2 mb-4" key={course.id} style={{ width: "25rem" }}>
               <img
